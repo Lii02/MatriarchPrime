eea4814629853b95ec57fef16655205209553c76
Lii02 <lukeinlow@gmail.com>
2025-05-28 11:28:37 -0400

    Added opengl initialization code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 static struct runtime_t {
     SDL_Window* window;
+    SDL_GLContext gl;
     int width, height;
 } rt;
 
@@ -14,10 +15,20 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     rt.height = 800;
     rt.window = SDL_CreateWindow("Game Starter Template", rt.width, rt.height, SDL_WINDOW_OPENGL);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    rt.gl = SDL_GL_CreateContext(rt.window);
+    SDL_GL_MakeCurrent(rt.window, rt.gl);
+    gladLoadGLES2((GLADloadfunc)SDL_GL_GetProcAddress);
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) {
+    glClear(GL_CLEAR_BUFFER | GL_DEPTH_BUFFER_BIT);
+    // Render/Update code here
+    SDL_GL_SwapWindow(rt.window);
     return SDL_APP_CONTINUE;
 }
 
@@ -31,5 +42,6 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    SDL_GL_DestroyContext(rt.gl);
     SDL_DestroyWindow(rt.window);
 }
